c7a36f21bfd3bbb71b93bef7c4c19fa00d1d244e
Taras Rudyi <trudyi@rebbix.com>
2015-06-04 17:40:26 +0300

    add empty implementations

---
+++ lngpok/lngpok.py
@@ -0,0 +1,32 @@
+def read_file(input_file):
+    lines = []
+    lines.append(input_file)
+    with open(input_file) as f:
+        for line in f:
+            if ' ' in line:
+                lines.append(map(int, line.split(' ')))
+            else:
+                lines.append(int(line))
+    return lines
+
+def write_file(output_file, value):
+    with open(output_file, 'w') as f:
+        f.write(str(value))
+
+
+def main():
+    input_file = "lngpok.in"
+    output_file = "lngpok.out"
+
+    # read data
+    lines = read_file(input_file)
+
+    print lines
+
+    result = 0
+
+    # write data
+    write_file(output_file, result)
+
+if __name__ == "__main__":
+    main()
