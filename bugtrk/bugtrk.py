2e4e886af8cc4144d8aa2ef475b7cb77859a710f
Taras Rudyi <trudyi@rebbix.com>
2015-06-24 19:56:45 +0300

    add solution

---
+++ bugtrk/bugtrk.py
@@ -0,0 +1,53 @@
+import math
+
+def read_file(input_file):
+    lines = []
+    lines.append(input_file)
+    with open(input_file) as f:
+        for line in f:
+            if ' ' in line:
+                lines.append(map(int, line.split(' ')))
+            else:
+                lines.append(int(line))
+    return lines
+
+def write_file(output_file, value):
+    with open(output_file, 'w') as f:
+        f.write(str(value))
+
+def main():
+    input_file = "bugtrk.in"
+    output_file = "bugtrk.out"
+
+    # read data
+    lines = read_file(input_file)
+
+    N = lines[1][0]
+    W = lines[1][1]
+    H = lines[1][2]
+
+    # find minimum side of the square
+    side = max(W, H, int(math.ceil(math.sqrt(N * W * H))))
+
+    # test side
+    small_side = min(W, H)
+    big_side = max(W, H)
+
+    elements_left = N
+    while True:
+        elements_in_row = (side - side % small_side) / small_side
+        number_of_rows = (side - side % big_side) / big_side
+        elements_left -= elements_in_row * number_of_rows
+        if elements_left <= 0:
+            break
+        else:
+            side = min(small_side * (elements_in_row + 1), big_side * (number_of_rows + 1))
+            elements_left = N
+
+    result = side
+
+    # write data
+    write_file(output_file, result)
+
+if __name__ == "__main__":
+    main()
