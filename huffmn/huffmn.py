734a8c15f11257d5894a6f4927d36e12ede56593
Taras Rudyi <taras.ruddy@gmail.com>
2015-07-29 20:12:18 +0300

    optimize memory usage on decompression

--- huffmn/huffmn.py
+++ huffmn/huffmn.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 
 MB = 1048576
 
-def bytes_from_file(filename, chunksize=8192):
+def bytes_from_file(filename, chunksize=1 * MB):
     with open(filename, 'r', 1 * MB) as f:
         while True:
             chunk = f.read(chunksize)
@@ -45,6 +45,12 @@ def build_codes(rates):
     code_it(codes, '', item_queue[0])
     return codes, item_queue
 
+def calculate_total_bits(rates, codes):
+    result = 0
+    for c in rates:
+        result += rates[c] * len(codes[c])
+    return result
+
 def compress(input_file, output_file):
     # init progress bar
     data_counter = 0
@@ -77,14 +83,11 @@ def compress(input_file, output_file):
         data_counter += 1
         print_progress(data_counter, data_size)
 
-    unused_bits = (8 - len(code_string)) % 8
-
     # we have uncompleted byte
-    if unused_bits:
+    total_bits = calculate_total_bits(rates, codes)
+    extra_bits = total_bits % 8
+    if extra_bits > 0:
         buff.append(int(code_string.ljust(8, '0'), 2))
-
-    # write last byte to identify unused bits
-    buff.append(unused_bits)
 
     if len(buff) > 0:
         file_out.write(buff)
@@ -96,9 +99,11 @@ def compress(input_file, output_file):
 def decompress(input_file, output_file):
     # read header
     file_in = open(input_file, 'rb')
+
     # 256 symbols, 4 bytes each
     header = file_in.read(1024)
     unpacked_header = struct.unpack('256I', header)
+
     # restore rates
     rates = defaultdict(int)
     for b, rate in enumerate(unpacked_header):
@@ -107,42 +112,53 @@ def decompress(input_file, output_file):
     # build huffman tree
     codes, tree = build_codes(rates)
 
-    # calculate and store binary codes for the 256 symbols
-    bin_codes = dict((k, ''.join(['1' if ((1 << i) & k) else '0' for i in range(7, -1, -1)])) for k in range(256))
-
-    # read encoded data
-    bit_string = ''.join([bin_codes[ord(byte)] for line in file_in for byte in line])
-    file_in.close()
-
-    # remove end code and extra bits
-    bit_string = bit_string[:-8 - int(bit_string[-8:], 2)]
+    total_bits = calculate_total_bits(rates, codes)
+    extra_bits = total_bits % 8
 
     # update data size for progress
     data_counter = 0
-    data_size = len(bit_string)
+    data_size = total_bits
     print_progress(data_counter, data_size)
 
     # create empty file
     file_out = open(output_file, 'w')
 
+    # read encoded data
+    bit_string = ''
+    bit_justify_by = 8
     root = tree[0]
     start = root
-    for b in bit_string:
-        data_counter += 1
-        print_progress(data_counter, data_size)
-        root = root.left if b == '0' else root.right
+    while True:
+        chunk1 = file_in.read(1 * MB)
+        chunk2 = file_in.read(1 * MB)
+        buff = bytearray(chunk1 + chunk2)
+        for i, byte in enumerate(buff):
+            # end of file - set extra bits justification
+            if chunk2 == '' and i + 1 == len(buff) and extra_bits > 0:
+                bit_justify_by = extra_bits
+            bit_string += bin(byte)[2:].rjust(bit_justify_by, '0')
+            for b in bit_string:
+                data_counter += 1
+                print_progress(data_counter, data_size)
+                root = root.left if b == '0' else root.right
 
-        if root.left is None and root.right is None:
-            # found a leaf node, you found a symbol then
-            file_out.write(root.item)
-            root = start
+                if root.left is None and root.right is None:
+                    # found a leaf node, you found a symbol then
+                    file_out.write(root.item)
+                    root = start
+                    code_len = len(codes[root.item])
+                    bit_string = bit_string[:code_len]
+        # end of file
+        if chunk2 == '':
+            break
 
+    file_in.close()
     file_out.close()
 
     print_progress(data_size, data_size)
 
-def print_progress(val, end_val, bar_length=100):
-    if val % (end_val / 100) == 0:
+def print_progress(val, end_val=1 * MB, bar_length=100):
+    if end_val > 1 * MB and val % (end_val / 100) == 0:
         percent = float(val) / end_val
         hashes = '#' * int(round(percent * bar_length))
         spaces = ' ' * (bar_length - len(hashes))
